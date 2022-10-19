86f1ef9adc139d0ce44e89aafe22a6773cd6601d
Kamil Kulka <kulka2190@gmail.com>
2022-10-19 15:21:16 +0200

    Add Workout composable added.

--- app/src/main/java/com/kamilkulka/randletics/screens/exercises/ExercisesScreen.kt
+++ app/src/main/java/com/kamilkulka/randletics/screens/exercises/ExercisesScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.kamilkulka.randletics.screens.main.Greeting
 
 @Composable
 fun ExercisesScreen(){
--- app/src/main/java/com/kamilkulka/randletics/screens/main/MainScreen.kt
+++ app/src/main/java/com/kamilkulka/randletics/screens/main/MainScreen.kt
@@ -1,22 +1,22 @@
 package com.kamilkulka.randletics.screens.main
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.sharp.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
@@ -63,13 +63,19 @@ fun MainScreen(
                         start = 5.dp,
                         end = 5.dp,
                         top = 5.dp,
-                        bottom = 100.dp),
+                        bottom = 100.dp
+                    ),
                     modifier = Modifier.fillMaxHeight()
                 ) {
 //                    items(workoutList.size) { index ->
 //                        WorkoutBox(workout = workoutList[index])
                     items(3) {
                         WorkoutBox()
+                    }
+                    item {
+                        AddWorkoutBox(){
+                            onAddWorkout()
+                        }
                     }
                 }
             }
@@ -93,39 +99,53 @@ fun WorkoutBox(
         val width = constraints.maxWidth
         val height = constraints.maxHeight
 
-        val startColorPoint = Offset(0f,height*0.2f)
-        val endColorPoint = Offset(width.toFloat(), height*0.6f)
+        val startColorPoint = Offset(0f, height * 0.2f)
+        val endColorPoint = Offset(width.toFloat(), height * 0.6f)
         //ColoredPath
-        val colorLayerPath = Path().apply{
-            moveTo(startColorPoint.x,startColorPoint.y)
-//            quadraticBezierTo(
-//                startColorPoint.x,
-//                startColorPoint.y,
-//                abs(startColorPoint.x + endColorPoint.x)/2f,
-//                abs(startColorPoint.y + endColorPoint.y)/2f
-//            )
-            lineTo(endColorPoint.x,endColorPoint.y)
-            lineTo(width.toFloat()+100f,height.toFloat()+100f)
-            lineTo(-100f,height.toFloat()+100f)
+        val colorLayerPath = Path().apply {
+            moveTo(startColorPoint.x, startColorPoint.y)
+            lineTo(endColorPoint.x, endColorPoint.y)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
             close()
         }
-        Canvas(modifier = Modifier.fillMaxSize()){
+        Canvas(modifier = Modifier.fillMaxSize()) {
             drawPath(path = colorLayerPath, color = Celadon)
         }
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(12.dp)){
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp)
+        ) {
             Text(text = "Title", modifier = Modifier.align(Alignment.TopStart))
         }
     }
 }
 
 @Composable
-fun AddWorkoutBox(){
-    Box(modifier = Modifier
-        .aspectRatio(1f)
-        .padding(6.dp)
-        .clip(RoundedCornerShape(corner = CornerSize(15.dp)))
-        .background(SageGreen))
+fun AddWorkoutBox(onAddWorkout: () -> Unit) {
+    Surface(
+        modifier = Modifier
+            .aspectRatio(1f)
+            .padding(6.dp)
+            .clickable {
+                onAddWorkout()
+            },
+        shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+        border = BorderStroke(width = 8.dp, color = SageGreen)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(DustyRose),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(imageVector = Icons.Sharp.Add,
+                contentDescription = "Add Workout",
+                modifier = Modifier.size(124.dp))
+            Text(text = "Add Workout")
+        }
+    }
 }
 
