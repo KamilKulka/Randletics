679a5c7ec9c0049fd61c41af78721529ca9cad75
Kamil Kulka <kulka2190@gmail.com>
2022-10-20 15:10:41 +0200

    NewWorkoutScreen UI (Scaffold, back button).

--- app/src/main/java/com/kamilkulka/randletics/screens/new_workout/NewWorkoutViewModel.kt
+++ app/src/main/java/com/kamilkulka/randletics/screens/new_workout/NewWorkoutViewModel.kt
@@ -2,8 +2,10 @@ package com.kamilkulka.randletics.screens.new_workout
 
 import androidx.lifecycle.ViewModel
 import com.kamilkulka.randletics.repository.WorkoutsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class NewWorkoutViewModel @Inject constructor(private val workoutsRepository: WorkoutsRepository) :
     ViewModel() {
 
