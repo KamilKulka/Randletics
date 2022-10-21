88ddaa3c850e75ddc1abf03280778f092d727136
Kamil Kulka <kulka2190@gmail.com>
2022-10-21 13:49:53 +0200

    Equipment types added.

---
+++ app/src/main/java/com/kamilkulka/randletics/models/EquipmentType.kt
@@ -0,0 +1,15 @@
+package com.kamilkulka.randletics.models
+
+enum class EquipmentType {
+    DUMBBELLS,
+    BARBELL,
+    FLAT_BENCH,
+    INCLINE_BENCH,
+    DECLINE_BENCH,
+    CURL_BENCH,
+    PULL_UP_BAR,
+    MID_PULL_UP_BAR,
+    RESISTANCE_BANDS,
+    LOW_CABLE_PULLEY,
+    TOP_CABLE_PULLEY
+}
