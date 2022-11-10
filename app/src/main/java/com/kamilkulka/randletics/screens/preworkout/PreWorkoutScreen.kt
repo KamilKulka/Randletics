d1daf346a3f3951bf3fba6b1ffa52f34607eac51
Kamil Kulka <kulka2190@gmail.com>
2022-11-11 00:16:12 +0100

    PreWorkoutScreen finisher, added: Difficulty, Equipment, exercises counter and drop bar.

--- app/src/main/java/com/kamilkulka/randletics/screens/preworkout/PreWorkoutScreen.kt
+++ app/src/main/java/com/kamilkulka/randletics/screens/preworkout/PreWorkoutScreen.kt
@@ -3,10 +3,15 @@ package com.kamilkulka.randletics.screens.preworkout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -16,8 +21,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.kamilkulka.randletics.ui.theme.Celadon
 import com.kamilkulka.randletics.ui.theme.DustyGreen
 import com.kamilkulka.randletics.ui.theme.SageGreen
+import com.kamilkulka.randletics.utils.RowWithIcon
 import java.util.*
 
 @Composable
@@ -69,8 +76,66 @@ fun PreWorkoutScreen(
                     fontWeight = FontWeight.SemiBold
                 )
             }
-        }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 12.dp, start = 12.dp, end = 12.dp, bottom = 80.dp)
+            ) {
 
+                RowWithIcon(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(4.dp),
+                    imageVector = Icons.Rounded.Leaderboard,
+                    text = "Difficulty: ${viewModel.getDifficultyStringOf(workoutId)}"
+                )
+
+
+                RowWithIcon(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(4.dp),
+                    imageVector = Icons.Rounded.FitnessCenter,
+                    text = viewModel.getEquipmentStringOf(workoutId)
+                )
+
+
+                RowWithIcon(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(4.dp),
+                    imageVector = Icons.Rounded.SettingsAccessibility,
+                    text = "Exercises: ${viewModel.countExercisesOf(workoutId)}"
+                )
+                if (viewModel.countExercisesOf(workoutId) > 0) {
+                    if (viewModel.exercisesDropdown.collectAsState().value) {
+                        Icon(imageVector = Icons.Rounded.ExpandLess,
+                            contentDescription = "Expand Exercises",
+                            modifier = Modifier.clickable { viewModel.setExercisesDropdown() })
+                    } else {
+                        Icon(imageVector = Icons.Rounded.ExpandMore,
+                            contentDescription = "Expand Exercises",
+                            modifier = Modifier.clickable { viewModel.setExercisesDropdown() })
+                    }
+                    if (viewModel.exercisesDropdown.collectAsState().value) {
+                        LazyColumn {
+                            items(items = viewModel.getExercisesList(workoutId)) { exercise ->
+                                Text(
+                                    modifier = Modifier.padding(
+                                        start = 20.dp,
+                                        top = 2.dp,
+                                        bottom = 2.dp,
+                                        end = 2.dp
+                                    ),
+                                    text = exercise.name,
+                                    color = Celadon
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
     }
 
