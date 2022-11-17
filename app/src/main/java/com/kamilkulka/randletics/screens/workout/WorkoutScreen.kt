148b9c8bb2b0dd8de8b72e0d4f1828e5ba8ec335
Kamil Kulka <kulka2190@gmail.com>
2022-11-18 00:30:53 +0100

    CountDown timer added to WorkoutScreen.kt.

--- app/src/main/java/com/kamilkulka/randletics/screens/workout/WorkoutScreen.kt
+++ app/src/main/java/com/kamilkulka/randletics/screens/workout/WorkoutScreen.kt
@@ -1,24 +1,52 @@
 package com.kamilkulka.randletics.screens.workout
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.kamilkulka.randletics.ui.theme.Celadon
+import com.kamilkulka.randletics.ui.theme.DustyGreen
+import com.kamilkulka.randletics.ui.theme.SageGreen
 
 @Composable
-fun WorkoutScreen(navController:NavController,
+fun WorkoutScreen(
+    navController: NavController,
     viewModel: WorkoutViewModel = hiltViewModel()
-)   {
-        Surface(modifier = Modifier.fillMaxSize().clickable { navController.popBackStack() }, color = Celadon) {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-                Text(text = "WorkoutScreen!")
+) {
+    Surface(modifier = Modifier
+        .fillMaxSize()
+        .clickable { navController.popBackStack() },
+        color = SageGreen
+    ) {
+        Column() {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(32.dp)
+                    .aspectRatio(ratio = 1f),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.fillMaxSize(),
+                    progress = viewModel.progressBar.collectAsState().value,
+                    color = DustyGreen,
+                    strokeWidth = 12.dp
+                )
+                
+                Text(text = viewModel.timeLeft.collectAsState().value.toString(), fontSize = 48.sp)
+            }
+            Button(onClick = { viewModel.startCounter() }) {
+
             }
         }
     }
+}
--- app/src/main/java/com/kamilkulka/randletics/screens/workout/WorkoutViewModel.kt
+++ app/src/main/java/com/kamilkulka/randletics/screens/workout/WorkoutViewModel.kt
@@ -1,9 +1,12 @@
 package com.kamilkulka.randletics.screens.workout
 
+import android.os.CountDownTimer
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.kamilkulka.randletics.repository.WorkoutsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,4 +14,29 @@ class WorkoutViewModel @Inject constructor(private val workoutsRepository: Worko
                                            private val savedStateHandle: SavedStateHandle):
     ViewModel() {
 
+    companion object{
+        const val SEC_SPAN = 45
+    }
+
+    private val _timeLeft = MutableStateFlow(0L)
+    val timeLeft = _timeLeft.asStateFlow()
+
+    private val _progressBar = MutableStateFlow(1f)
+    val progressBar = _progressBar.asStateFlow()
+
+        private val counter = object: CountDownTimer(SEC_SPAN *1000L,1){
+        override fun onTick(millisUntilFinished: Long) {
+            _timeLeft.value = millisUntilFinished/1000
+            _progressBar.value = millisUntilFinished.toFloat()/ (SEC_SPAN*1000).toFloat()
+        }
+
+        override fun onFinish() {
+
+        }
+    }
+
+
+    fun startCounter(){
+        counter.start()
+    }
 }
