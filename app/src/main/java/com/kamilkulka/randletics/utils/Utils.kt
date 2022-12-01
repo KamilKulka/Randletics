9148c2e0c351853819b82c6d46c1e614fbdd8bcb
Kamil Kulka <kulka2190@gmail.com>
2022-12-02 00:22:53 +0100

    FilterDropDown Composable added.

--- app/src/main/java/com/kamilkulka/randletics/utils/Utils.kt
+++ app/src/main/java/com/kamilkulka/randletics/utils/Utils.kt
@@ -1,22 +1,25 @@
 package com.kamilkulka.randletics.utils
 
-import android.view.ViewGroup
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowDown
+import androidx.compose.material.icons.rounded.KeyboardArrowUp
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.PopupProperties
 import com.kamilkulka.randletics.ui.theme.LimeGreen
 import com.kamilkulka.randletics.ui.theme.DustyGreen
 
@@ -89,20 +92,58 @@ fun AlertPopUp(
 }
 
 @Composable
-fun WebViewBox(modifier: Modifier = Modifier, url: String) {
-    AndroidView(modifier = modifier, factory = {
-        WebView(it).apply {
-            settings.javaScriptEnabled = true
-            layoutParams = ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-            )
-            webViewClient = WebViewClient()
-            loadUrl(url)
-        }
+fun TopIconButton(imageVector: ImageVector, onClick: () -> Unit) {
+    IconButton(onClick = onClick, modifier = Modifier.padding(24.dp)) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = null,
+            tint = Color.Black,
+            modifier = Modifier.size(38.dp)
+        )
+    }
+}
 
-    },
-        update = {
-            it.loadUrl(url)
-        })
+@Composable
+fun FilterDropDown(
+    modifier: Modifier = Modifier,
+    filterCategoryName: String,
+    chosenElement: String,
+    expanded: Boolean,
+    onDropButtonClick: () -> Unit,
+    onDismissRequest: () -> Unit,
+    content: @Composable ColumnScope.() -> Unit
+) {
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = filterCategoryName,
+            fontSize = 20.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center
+        )
+        Box {
+            Row {
+                Text(
+                    text = chosenElement,
+                    fontSize = 20.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Light,
+                    textAlign = TextAlign.Center
+                )
+                Icon(
+                    imageVector = if (expanded) Icons.Rounded.KeyboardArrowUp else Icons.Rounded.KeyboardArrowDown,
+                    contentDescription = null,
+                    modifier = Modifier.clickable { onDropButtonClick() })
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = onDismissRequest,
+                content = content
+            )
+        }
+    }
 }
